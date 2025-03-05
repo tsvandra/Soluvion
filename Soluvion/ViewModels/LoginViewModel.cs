47f927b0f1365d8fab0ef3333f2e992b6cce91b6
tsvandra <tsvandra@ccn.sk>
2025-03-05 13:44:32 +0100

    elso commit

---
+++ Soluvion/ViewModels/LoginViewModel.cs
@@ -0,0 +1,39 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Soluvion.Models;
+
+namespace Soluvion.ViewModels
+{
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+        private string _username;
+        private string _password;
+
+        public string Username
+        {
+            get => _username;
+            set
+            {
+                _username = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
